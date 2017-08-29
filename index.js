4486e18cd935877497ff17fd98c397fbd0ca4ecb
Daniel Lim <ddanielnp@hotmail.com>
2017-08-29 13:21:01 +0800

    setup post-test deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose')
 var tacosController = require('./controllers/tacos_controller')
 var app = express()
 
-mongoose.connect('mongodb://localhost/taco-api')
+mongoose.connect('mongodb://travis_mocha:mocha@ds161873.mlab.com:61873/travis_mocha_api_starter')
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 app.use('/tacos', tacosController)
 
 var server = app.listen(process.env.PORT || 3000)
-console.log('Server UP localhost:3000')
+console.log('Server UP localhost', server)
 
 // we export the running server so we can use it in testing
 module.exports = server
